8a52ecc47f56c9271ef17369875d7679c6c45c31
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-06-28 10:39:15 -0500

    changes

--- candycrush/src/App.js
+++ candycrush/src/App.js
@@ -1,9 +1,64 @@
-import "./App.css";
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const width = 8;
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+];
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  
+  const checkForColumnOfThree = () => {
+      for (let i = 0; i < 47; i++ ){
+        const columnOfThree = [i, i + width, i + width * 2]
+        const decidedColor = currentColorArrangment[i]
+
+        if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+          columnOfThree.forEach(square => currentColorArrangement[square] = '');
+        }
+      }
+  }
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width*width; i++){
+      const randomColor = candyColors[Math.floor(Math.random()* candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  }
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect (() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+    }, 100);
+    
+    return () => clearInterval(timer);
+
+  }, [checkForColumnOfThree]);
+  
   return (
-    <div>App</div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
+          />
+        ))}
+      </div>
+    </div>
+
   )
 }
 
