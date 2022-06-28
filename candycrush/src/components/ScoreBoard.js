78a53fe0aac0d26077792d1c0a504a049b287796
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-06-28 11:38:01 -0500

    finished

---
+++ candycrush/src/components/ScoreBoard.js
@@ -0,0 +1,11 @@
+
+const ScoreBoard = ({ score }) => {
+    return (
+      <div className="score-board">
+        <h1>Score:</h1>
+        <h2>{score}</h2>
+      </div>
+    )
+  }
+  
+  export default ScoreBoard;
